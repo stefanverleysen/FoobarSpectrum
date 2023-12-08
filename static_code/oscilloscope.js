fb7abf8d43cef884ae23779625e5e6f3e730f4df
Stef @ Asan Digital LLC <stef@asan.digital>
2023-12-08 13:31:53 -0500

    Update oscilloscope.js

--- static_code/oscilloscope.js
+++ static_code/oscilloscope.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioContext = new AudioContext();
     let analyser = audioContext.createAnalyser();
+    let dataArray;
     let isDrawing = false;
     let drawVisual;
 
@@ -17,22 +18,67 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     function drawOscilloscope() {
         if (!isDrawing) return;
-        // Drawing logic (to be implemented based on specific visualization needs)
+        drawVisual = requestAnimationFrame(drawOscilloscope);
+
+        analyser.getByteTimeDomainData(dataArray);
+        oscilloscopeCtx.fillStyle = 'rgb(200, 200, 200)';
+        oscilloscopeCtx.fillRect(0, 0, oscilloscopeCanvas.width, oscilloscopeCanvas.height);
+
+        oscilloscopeCtx.lineWidth = 2;
+        oscilloscopeCtx.strokeStyle = 'rgb(0, 0, 0)';
+        oscilloscopeCtx.beginPath();
+
+        var sliceWidth = oscilloscopeCanvas.width * 1.0 / analyser.fftSize;
+        var x = 0;
+
+        for (var i = 0; i < analyser.fftSize; i++) {
+            var v = dataArray[i] / 128.0;
+            var y = v * oscilloscopeCanvas.height / 2;
+
+            if (i === 0) {
+                oscilloscopeCtx.moveTo(x, y);
+            } else {
+                oscilloscopeCtx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        oscilloscopeCtx.lineTo(oscilloscopeCanvas.width, oscilloscopeCanvas.height / 2);
+        oscilloscopeCtx.stroke();
     }
 
     function startOscilloscope() {
         if (isDrawing) return;
         isDrawing = true;
-        drawVisual = requestAnimationFrame(drawOscilloscope);
-        // User-initiated start logic (connect to audio source, etc.)
+
+        analyser.fftSize = 2048;
+        dataArray = new Uint8Array(analyser.fftSize);
+
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+            const source = audioContext.createMediaStreamSource(stream);
+            source.connect(analyser);
+            drawVisual = requestAnimationFrame(drawOscilloscope);
+        }).catch(err => {
+            console.error('Error accessing microphone:', err);
+        });
     }
 
     function stopOscilloscope() {
         if (!isDrawing) return;
         isDrawing = false;
         cancelAnimationFrame(drawVisual);
-        // Stop logic (disconnect from audio source, etc.)
+        analyser.disconnect();
+        audioContext.close();
     }
+
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+    const fullscreenButton = document.getElementById('fullscreenButton');
+
+    startButton.addEventListener('click', startOscilloscope);
+    stopButton.addEventListener('click', stopOscilloscope);
+    fullscreenButton.addEventListener('click', toggleFullscreen);
 
     function toggleFullscreen() {
         if (!document.fullscreenElement) {
@@ -43,12 +89,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
             document.exitFullscreen();
         }
     }
-
-    const startButton = document.getElementById('startButton');
-    const stopButton = document.getElementById('stopButton');
-    const fullscreenButton = document.getElementById('fullscreenButton');
-
-    startButton.addEventListener('click', startOscilloscope);
-    stopButton.addEventListener('click', stopOscilloscope);
-    fullscreenButton.addEventListener('click', toggleFullscreen);
 });
