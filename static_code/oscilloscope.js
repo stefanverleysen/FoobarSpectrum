f3041fb6ab5e93f44cf798c746ca8ad35d984e03
Stef @ Asan Digital LLC <stef@asan.digital>
2023-12-08 17:25:34 -0500

    Update oscilloscope.js

--- static_code/oscilloscope.js
+++ static_code/oscilloscope.js
@@ -1,87 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  audioContext = new (window.AudioContext || window.webkitAudioContext)();
-   
-  analyser = audioContext.createAnalyser();
-   
-  analyser.fftSize = 8192;
-   
-  dataArray = new Uint8Array(analyser.fftSize);
+  let audioContext;
 
-  isDrawing = false;
- 
-  drawVisual;
+  let analyser;
 
-  oscilloscopeCanvas = document.getElementById('oscilloscopeCanvas');
+  let dataArray;
+
+  let isDrawing = false;
    
-  oscilloscopeCtx = oscilloscopeCanvas.getContext('2d');
+  let drawVisual;
 
-  dpr = window.devicePixelRatio || 1;
+  const oscilloscopeCanvas = document.getElementById('oscilloscopeCanvas');
+  
+  const oscilloscopeCtx = oscilloscopeCanvas.getContext('2d');
+
+  let dpr = window.devicePixelRatio || 1;
 
   oscilloscopeCanvas.width = oscilloscopeCanvas.offsetWidth * dpr;
-   
+  
   oscilloscopeCanvas.height = oscilloscopeCanvas.offsetHeight * dpr;
 
   oscilloscopeCtx.scale(dpr, dpr);
-  
-  lineThickness = 2;
+
+  let lineThickness = 2;
 
   function getRandomCyberpunkColor() {
 
-    colors = ["#FF00FF", "#00FFFF", "#00FF00", "#FF0000","#FFFF00"];
+    const colors = ["#FF00FF", "#00FFFF", "#00FF00", "#FF0000", "#FFFF00"];
 
     return colors[Math.floor(Math.random() * colors.length)];
 
   }
 
   function resizeCanvas() {
-
-    if (document.fullscreenElement) {
-
+    if (document.fullscreenElement) {  
       oscilloscopeCanvas.width = window.screen.width * dpr;
-
       oscilloscopeCanvas.height = window.screen.height * dpr;
-
-    } else {
-
+    } else {   
       oscilloscopeCanvas.width = oscilloscopeCanvas.offsetWidth * dpr;
-
       oscilloscopeCanvas.height = oscilloscopeCanvas.offsetHeight * dpr;
-
     }
-
     oscilloscopeCtx.scale(dpr, dpr);
   
     if (isDrawing) {
-
       drawOscilloscope();
-
     }
-
   }
 
   function onFullScreenChange() {
-   
     resizeCanvas();
-  
   }
-
+  
   oscilloscopeCanvas.addEventListener('click', toggleFullscreen);
   
   oscilloscopeCanvas.addEventListener('dblclick', () => {
-
     if (document.fullscreenElement) {
-
       document.exitFullscreen();
-
-    }
-
+    } 
   });
 
   document.addEventListener('fullscreenchange', onFullScreenChange);
 
   function drawOscilloscope() {
-
+    
     if (!isDrawing) return;
 
     drawVisual = requestAnimationFrame(drawOscilloscope);
@@ -89,42 +70,85 @@ document.addEventListener('DOMContentLoaded', () => {
     analyser.getByteTimeDomainData(dataArray);
 
     oscilloscopeCtx.fillStyle = 'rgb(0, 0, 0)';
-
     oscilloscopeCtx.fillRect(0, 0, oscilloscopeCanvas.width, oscilloscopeCanvas.height);
 
     oscilloscopeCtx.lineWidth = lineThickness;
-
     oscilloscopeCtx.strokeStyle = getRandomCyberpunkColor();
 
     oscilloscopeCtx.beginPath();
 
     var sliceWidth = oscilloscopeCanvas.width / dataArray.length;
-    
     var x = 0;
 
     for (var i = 0; i < dataArray.length; i++) {
 
-      var v = dataArray[i] / 128.0;  
-      var y = v * oscilloscopeCanvas.height; 
+      var v = dataArray[i] / 128.0;
+      var y = v * oscilloscopeCanvas.height / 2;
 
       if (i === 0) {
-      
         oscilloscopeCtx.moveTo(x, y);
-      
       } else {
-
-        oscilloscopeCtx.lineTo(x, y);
-
+        oscilloscopeCtx.lineTo(x, y);  
       }
 
       x += sliceWidth;
     }
 
     oscilloscopeCtx.lineTo(oscilloscopeCanvas.width, oscilloscopeCanvas.height/2);
-    
     oscilloscopeCtx.stroke();
-
+  
   }
 
+  function startOscilloscope() {
+   
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 8192;
+
+    dataArray = new Uint8Array(analyser.fftSize);
+    analyser.getByteTimeDomainData(dataArray);
+
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+
+      const source = audioContext.createMediaStreamSource(stream);
+      source.connect(analyser);
+
+      isDrawing = true; 
+      drawVisual = requestAnimationFrame(drawOscilloscope);
+
+    }).catch(err => {
+      console.error('Error accessing microphone:', err);
+    });
+  
+  }
+
+  function stopOscilloscope() {
+
+    if (!isDrawing) return;
+
+    isDrawing = false;
+    cancelAnimationFrame(drawVisual); 
+
+    analyser.disconnect();
+    audioContext.close();
+  
+  }
+
+  function toggleFullscreen() {
+    if (!document.fullscreenElement) {   
+      oscilloscopeCanvas.requestFullscreen().catch(err => {  
+        alert('Error attempting to enable fullscreen mode: ' + err.message);
+      });
+    } else {
+      document.exitFullscreen(); 
+    }
+  }
+
+  const startButton = document.getElementById('startButton');
+  const stopButton = document.getElementById('stopButton');
+  
+  startButton.addEventListener('click', startOscilloscope);
+  stopButton.addEventListener('click', stopOscilloscope);
 
 });
