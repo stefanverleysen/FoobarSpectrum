d332427f2a63e73193ba9ffc1b60945982e3c90e
Stef @ Asan Digital LLC <stef@asan.digital>
2023-12-08 19:47:02 -0500

    Update visualizer.js

--- static_code/visualizer.js
+++ static_code/visualizer.js
@@ -1,11 +1,9 @@
 // Get the audio visualizer canvas and its context
 const canvas = document.getElementById('audioVisualizerCanvas');
 const canvasCtx = canvas.getContext('2d');
-const fftSizeSlider = document.getElementById('fftSizeSlider');
 const smoothingSlider = document.getElementById('smoothingSlider');
 const barCountSlider = document.getElementById('barCountSlider');
 const barCountValue = document.getElementById('barCountValue');
-const fftSizeValue = document.getElementById('fftSizeValue');
 const smoothingValue = document.getElementById('smoothingValue');
 
 let audioContext;
@@ -15,90 +13,80 @@ let animationId;
 let barCount = 100;
 let backgroundColor = '#000'; // Background color
 
-// Function to generate a random cyberpunk color in hex format
+// Function to generate a random cyberpunk color
 function getRandomCyberpunkColor() {
-    const cyberpunkColors = ["#FF00FF", "#00FFFF", "#00FF00", "#FF0000", "#FFFF00"];
-    return cyberpunkColors[Math.floor(Math.random() * cyberpunkColors.length)];
+    const colors = ["#FF00FF", "#00FFFF", "#00FF00", "#FF0000", "#FFFF00"];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 // Calculate the width of each bar based on the screen width and number of bars
 function calculateBarWidth() {
-    const screenWidth = window.innerWidth; // Get the screen width
-    return screenWidth / barCount;
+    return window.innerWidth / barCount;
 }
 
 // Update the canvas resolution and initial bar colors
 function setupCanvasResolution() {
     const pixelRatio = window.devicePixelRatio || 1;
-    const barWidth = calculateBarWidth(); // Calculate the bar width dynamically
+    const barWidth = calculateBarWidth();
     canvas.width = barWidth * barCount * pixelRatio;
     canvas.height = canvas.offsetHeight * pixelRatio;
     canvasCtx.scale(pixelRatio, pixelRatio);
-    canvasCtx.fillStyle = backgroundColor; // Set the background color
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height); // Fill canvas with background color
+    canvasCtx.fillStyle = backgroundColor;
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-// Function to set initial bar colors to a random cyberpunk color
+// Set initial bar colors
 function setInitialBarColors() {
-    const randomColor = getRandomCyberpunkColor();
-    const barWidth = calculateBarWidth(); // Calculate bar width dynamically
+    const barWidth = calculateBarWidth();
     let x = 0;
-
     for (let i = 0; i < barCount; i++) {
-        canvasCtx.fillStyle = randomColor; // Set bar color to random cyberpunk color
+        canvasCtx.fillStyle = getRandomCyberpunkColor();
         canvasCtx.fillRect(x, 0, barWidth, canvas.height);
         x += barWidth;
     }
 }
 
-// Rest of your code remains the same...
+// Define the draw function globally
+function draw() {
+    animationId = requestAnimationFrame(draw);
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
+    const barWidth = calculateBarWidth();
+    let x = 0;
+    for (let i = 0; i < barCount; i++) {
+        const barHeight = dataArray[i] * (canvas.height / 256);
+        canvasCtx.fillStyle = getRandomCyberpunkColor();
+        canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+}
 
-
-// Function to start visualization (you can add your visualization logic here)
+// Start visualization
 function startVisualization() {
     if (!navigator.mediaDevices.getUserMedia) {
         alert('Your browser does not support getUserMedia API');
         return;
     }
-
     navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
         audioContext = new (window.AudioContext || window.webkitAudioContext)();
         microphone = audioContext.createMediaStreamSource(stream);
         analyser = audioContext.createAnalyser();
-        analyser.fftSize = parseInt(fftSizeSlider.value, 10);
         analyser.smoothingTimeConstant = parseFloat(smoothingSlider.value);
 
         microphone.connect(analyser);
         setupCanvasResolution();
         setInitialBarColors();
-
-        // Define the draw function globally
-        function draw() {
-            animationId = requestAnimationFrame(draw);
-            const bufferLength = analyser.frequencyBinCount;
-            const dataArray = new Uint8Array(bufferLength);
-            analyser.getByteFrequencyData(dataArray);
-            canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
-            const barWidth = calculateBarWidth(); // Calculate bar width dynamically
-            let x = 0;
-
-            for (let i = 0; i < barCount; i++) {
-                const barHeight = dataArray[i] * (canvas.height / 256);
-                canvasCtx.fillStyle = getRandomCyberpunkColor(); // Set bar color to random cyberpunk color
-                canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-                x += barWidth;
-            }
-        }
-
-        draw(); // Call the draw function when starting visualization
+        draw();
     }).catch(error => {
         console.error('Error accessing microphone:', error);
         alert('Error accessing microphone: ' + error.message);
     });
 }
 
+// Stop visualization
 function stopVisualization() {
     if (microphone) {
         microphone.disconnect();
@@ -113,71 +101,23 @@ function stopVisualization() {
     }
 }
 
-// Add the new code for mouse and keyboard interactions here
-
-let isMouseOverCanvas = false;
-let isFullscreen = false;
-let isStopped = true;
-
-// Event listener for mouseover and mouseout on the canvas
-canvas.addEventListener('mouseover', () => {
-    isMouseOverCanvas = true;
-});
-
-canvas.addEventListener('mouseout', () => {
-    isMouseOverCanvas = false;
-});
-// Event listener for double click to toggle fullscreen
-canvas.addEventListener('dblclick', () => {
-    if (!isFullscreen) {
-        // Toggle to fullscreen mode
-        canvas.requestFullscreen();
+// Toggle Fullscreen
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        canvas.requestFullscreen().catch(err => {
+            alert(`Error attempting to enable fullscreen: ${err.message}`);
+        });
     } else {
-        // Exit fullscreen mode
         document.exitFullscreen();
     }
-});
-
-
-// Function to change bar colors to a random cyberpunk color
-function changeBarColors() {
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    analyser.getByteFrequencyData(dataArray);
-
-    const barWidth = calculateBarWidth(); // Calculate bar width dynamically
-    let x = 0;
-
-    // Define an array of cyberpunk colors
-    const cyberpunkColors = ["#FF00FF", "#00FFFF", "#00FF00", "#FF0000", "#FFFF00"];
-
-    // Get a random cyberpunk color from the array
-    const randomColor = cyberpunkColors[Math.floor(Math.random() * cyberpunkColors.length)];
-
-    for (let i = 0; i < barCount; i++) {
-        const barHeight = dataArray[i] * (canvas.height / 256);
-        canvasCtx.fillStyle = randomColor; // Set bar color to the random color
-        canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-
-        x += barWidth + 1;
-    }
-    
-    // Call the draw function to update the visualization
-    draw();
 }
 
-// Update the bar count value and display it
+// Update value displays
 function updateBarCountValue() {
     barCount = parseInt(barCountSlider.value, 10);
     barCountValue.textContent = barCount;
 }
 
-// Update the FFT size value and display it
-function updateFFTSizeValue() {
-    fftSizeValue.textContent = fftSizeSlider.value;
-}
-
-// Update the smoothing value and display it
 function updateSmoothingValue() {
     smoothingValue.textContent = smoothingSlider.value;
 }
@@ -187,258 +127,9 @@ document.getElementById('startVisualization').addEventListener('click', startVis
 document.getElementById('stopVisualization').addEventListener('click', stopVisualization);
 barCountSlider.addEventListener('input', updateBarCountValue);
 smoothingSlider.addEventListener('input', updateSmoothingValue);
-
-// Event listener for keydown to handle key presses
-document.addEventListener('keydown', (event) => {
-    if (isMouseOverCanvas) {
-        if (event.key === 'Enter' && !isFullscreen) {
-            // Enter key pressed, toggle fullscreen mode
-            isFullscreen = true;
-            canvas.requestFullscreen(); // Use appropriate method for fullscreen
-        } else if (event.key === 'Escape' && isFullscreen) {
-            // Escape key pressed, exit fullscreen mode
-            isFullscreen = false;
-            document.exitFullscreen(); // Use appropriate method to exit fullscreen
-        } else if (event.key === ' ' && !isStopped) {
-            // Space key pressed, stop or start visualization
-            isStopped = true;
-            stopVisualization();
-        } else if (event.key === ' ' && isStopped) {
-            // Space key pressed, stop or start visualization
-            isStopped = false;
-            startVisualization();
-    }
-});
-
+canvas.addEventListener('dblclick', toggleFullscreen);
 
 // Initialize values
 updateBarCountValue();
-updateFFTSizeValue();
 updateSmoothingValue();
 setupCanvasResolution();
-
-document.addEventListener('dblclick', function() {
-    if (document.fullscreenElement) {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) { /* Safari */
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { /* IE11 */
-            document.msExitFullscreen();
-        }
-    }
-});
-
-// Adjust the canvas size on entering and exiting fullscreen
-document.addEventListener('fullscreenchange', adjustCanvasSize);
-
-function adjustCanvasSize() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (document.fullscreenElement) {
-        canvas.width = window.screen.width;
-        canvas.height = window.screen.height;
-    } else {
-        canvas.width = canvas.parentElement.offsetWidth;
-        canvas.height = canvas.parentElement.offsetHeight;
-    }
-    // Additional code may be required here to re-initialize or redraw the canvas content after resizing
-}
-
-// Function to handle entering fullscreen, adjusted for mobile compatibility
-function enterFullScreen() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (canvas.requestFullscreen) {
-        canvas.requestFullscreen();
-    } else if (canvas.mozRequestFullScreen) { // Firefox
-        canvas.mozRequestFullScreen();
-    } else if (canvas.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-        canvas.webkitRequestFullscreen();
-    } else if (canvas.msRequestFullscreen) { // IE/Edge
-        canvas.msRequestFullscreen();
-    }
-}
-
-// Function to handle exiting fullscreen
-function exitFullScreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { // Safari
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { // IE11
-        document.msExitFullscreen();
-    }
-}
-
-// Event listener for canvas to enter fullscreen on click
-document.getElementById('audioVisualizerCanvas').addEventListener('click', enterFullScreen);
-
-// Adjust the canvas size on entering and exiting fullscreen
-document.addEventListener('fullscreenchange', adjustCanvasSize);
-
-function adjustCanvasSize() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (document.fullscreenElement) {
-        canvas.width = window.screen.width;
-        canvas.height = window.screen.height;
-    } else {
-        canvas.width = canvas.parentElement.offsetWidth;
-        canvas.height = canvas.parentElement.offsetHeight;
-    }
-    // Additional code may be required here to re-initialize or redraw the canvas content after resizing
-}
-
-// Add double-click event listener to toggle fullscreen mode
-document.addEventListener('dblclick', function() {
-    if (document.fullscreenElement) {
-        exitFullScreen();
-    } else {
-        enterFullScreen();
-    }
-});
-
-// Adjust the canvas size on orientation change
-window.addEventListener('orientationchange', function() {
-    adjustCanvasSize();
-    // Additional code may be required here to re-initialize or redraw the canvas content after resizing
-}, false);
-
-// Existing adjustCanvasSize function from previous updates
-function adjustCanvasSize() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (document.fullscreenElement) {
-        // Wait for the orientation change to complete before calculating new size
-        setTimeout(function() {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-        }, 150); // Delay to allow the browser to complete the orientation change
-    } else {
-        canvas.width = canvas.parentElement.offsetWidth;
-        canvas.height = canvas.parentElement.offsetHeight;
-    }
-    // Additional code may be required here to re-initialize or redraw the canvas content after resizing
-}
-
-// Refine the enterFullScreen function
-function enterFullScreen() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (!document.fullscreenElement) {
-        if (canvas.requestFullscreen) {
-            canvas.requestFullscreen();
-        } else if (canvas.mozRequestFullScreen) { // Firefox
-            canvas.mozRequestFullScreen();
-        } else if (canvas.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-            canvas.webkitRequestFullscreen();
-        } else if (canvas.msRequestFullscreen) { // IE/Edge
-            canvas.msRequestFullscreen();
-        }
-        // After entering fullscreen, ensure the canvas size is correct
-        setTimeout(adjustCanvasSize, 100); // Delay to account for browser timing
-    }
-}
-
-// Refine the exitFullScreen function
-function exitFullScreen() {
-    if (document.fullscreenElement) {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) { // Safari
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { // IE11
-            document.msExitFullscreen();
-        }
-        // After exiting fullscreen, ensure the canvas size is correct
-        setTimeout(adjustCanvasSize, 100); // Delay to account for browser timing
-    }
-}
-
-// Double-click event listener to toggle fullscreen mode
-document.addEventListener('dblclick', function() {
-    if (document.fullscreenElement) {
-        exitFullScreen();
-    } else {
-        enterFullScreen();
-    }
-});
-
-// Custom function to handle double-tap for iOS devices
-let lastTap = 0;
-function handleDoubleTap(event) {
-    const currentTime = new Date().getTime();
-    const tapLength = currentTime - lastTap;
-    if (tapLength < 500 && tapLength > 0) {
-        // Detected a double-tap, toggle fullscreen
-        if (document.fullscreenElement) {
-            exitFullScreen();
-        } else {
-            enterFullScreen();
-        }
-    }
-    lastTap = currentTime;
-}
-
-// Add touch event listeners for iOS devices
-document.getElementById('audioVisualizerCanvas').addEventListener('touchend', handleDoubleTap);
-
-// Fullscreen functions
-function enterFullScreen() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (!document.fullscreenElement) {
-        if (canvas.requestFullscreen) {
-            canvas.requestFullscreen();
-        } else if (canvas.mozRequestFullScreen) {
-            canvas.mozRequestFullScreen();
-        } else if (canvas.webkitRequestFullscreen) {
-            canvas.webkitRequestFullscreen();
-        } else if (canvas.msRequestFullscreen) {
-            canvas.msRequestFullscreen();
-        }
-        setTimeout(adjustCanvasSize, 100);
-    }
-}
-
-function exitFullScreen() {
-    if (document.fullscreenElement) {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-        setTimeout(adjustCanvasSize, 100);
-    }
-}
-
-// Adjust the canvas size on entering and exiting fullscreen
-document.addEventListener('fullscreenchange', adjustCanvasSize);
-
-function adjustCanvasSize() {
-    var canvas = document.getElementById('audioVisualizerCanvas');
-    if (document.fullscreenElement) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    } else {
-        canvas.width = canvas.parentElement.offsetWidth;
-        canvas.height = canvas.parentElement.offsetHeight;
-    }
-}
-
-// Custom function to request fullscreen for iOS devices
-function requestFullscreenIOS(element) {
-    if (element.webkitEnterFullscreen) {
-        element.webkitEnterFullscreen(); // For iPhone
-    } else if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    }
-}
-
-// Use touchstart event to trigger fullscreen on iOS
-document.getElementById('audioVisualizerCanvas').addEventListener('touchstart', function() {
-    requestFullscreenIOS(this);
-});
